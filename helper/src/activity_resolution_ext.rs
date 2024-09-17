7a1dcf3f5d1e36a0fafd76f66680202b3569faa2
Lemon <bxb100@outlook.com>
2024-09-17 15:03:52 +0800

    chore

--- helper/src/activity_resolution_ext.rs
+++ helper/src/activity_resolution_ext.rs
@@ -1,7 +1,5 @@
-use log::debug;
-
 use temporal_sdk_core::protos::coresdk::activity_result::{
-    activity_resolution::Status::Completed, ActivityResolution,
+    activity_resolution::Status::Completed, ActivityResolution, Success,
 };
 
 pub trait ActivityResolutionExt {
@@ -15,16 +13,14 @@ impl ActivityResolutionExt for ActivityResolution {
     where
         T: serde::Deserialize<'a>,
     {
-        if let Some(Completed(result)) = &self.status {
-            if let Some(payload) = &result.result {
-                // let data = from_utf8(&payload.data).unwrap();
-                let result: T = serde_json::from_slice(&payload.data)?;
-                // println!("Activity completed with: {:#?}", string_result.to_owned());
-                return Ok(result);
-            }
-        } else {
-            debug!("Activity failed with {:?}", self.status);
+        if let Some(Completed(Success {
+            result: Some(ref payload),
+        })) = self.status
+        {
+            let result: T = serde_json::from_slice(&payload.data)?;
+            return Ok(result);
         }
-        Err(anyhow::anyhow!("Activity failed"))
+
+        Err(anyhow::anyhow!("Activity failed with {:?}", self.status))
     }
 }
