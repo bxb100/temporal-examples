d79ab7596ee08da614247bc0ecfe06e0bc365857
Lemon <bxb100@outlook.com>
2024-09-18 22:10:56 +0800

    feat: add new helper function

--- hello-world/src/worker.rs
+++ hello-world/src/worker.rs
@@ -1,27 +1,19 @@
 use crate::activities::greet;
 use crate::workflows::example;
 use anyhow::Result;
-use helper::core_runtime;
-use helper::util::client::get_client;
 use helper::worker_ext::WorkerExt;
-use std::sync::Arc;
 use temporal_sdk::Worker;
-use temporal_sdk_core::{init_worker, WorkerConfigBuilder};
+use temporal_sdk_core::WorkerConfigBuilder;
 
 pub async fn start_worker() -> Result<()> {
-    let client = get_client().await?;
-
     let worker_config = WorkerConfigBuilder::default()
         .namespace("default")
         .task_queue("hello-world")
         .worker_build_id("temporal-examples-rs")
         .build()?;
 
-    let core_worker = init_worker(core_runtime(), worker_config, client)?;
-
-    let mut worker = Worker::new_from_core(Arc::new(core_worker), "hello-world".to_string());
-
-    worker
+    Worker::single(worker_config)
+        .await?
         .register_act(greet)
         .register_workflow("example", example)
         .run()
--- helper/src/worker_ext.rs
+++ helper/src/worker_ext.rs
@@ -1,6 +1,12 @@
+use std::future::Future;
+use std::sync::Arc;
 use temporal_sdk::{IntoActivityFunc, Worker, WorkflowFunction};
+use temporal_sdk_core::init_worker;
+use temporal_sdk_core_api::worker::WorkerConfig;
 
 pub trait WorkerExt {
+    fn single(worker_config: WorkerConfig) -> impl Future<Output = anyhow::Result<Worker>> + Send;
+
     fn register_act<T, A, R, O>(&mut self, t: T) -> &mut Self
     where
         T: IntoActivityFunc<A, R, O>;
@@ -17,6 +23,14 @@ fn tuple_type_with_name<T>(t: T) -> (&'static str, T) {
 }
 
 impl WorkerExt for Worker {
+    async fn single(worker_config: WorkerConfig) -> anyhow::Result<Worker> {
+        let task_queue = worker_config.task_queue.clone();
+
+        let client = super::get_client().await?;
+        let core_worker = init_worker(super::core_runtime(), worker_config, client)?;
+        Ok(Worker::new_from_core(Arc::new(core_worker), task_queue))
+    }
+
     fn register_act<T, A, R, O>(&mut self, t: T) -> &mut Self
     where
         T: IntoActivityFunc<A, R, O>,
